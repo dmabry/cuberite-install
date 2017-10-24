24907c3a67475bdd13cce80983af33e84c211e5e
David Mabry <dmabry@ena.com>
2017-10-24 10:24:43 -0500

    update

--- install_cuberite_centos7.sh
+++ install_cuberite_centos7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum install -y clang git cmake make gcc glibc-devel screen
+sudo yum install -y clang git cmake make gcc glibc-devel screen libstdc*
 git clone --recursive https://github.com/cuberite/cuberite.git
 cd cuberite
 cmake . -DCMAKE_BUILD_TYPE=RELEASE
