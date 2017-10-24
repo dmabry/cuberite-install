b3c47aeb7d3475ea16c5ebefd6575b8d429fef2e
David Mabry <dmabry@ena.com>
2017-10-24 10:36:34 -0500

    added install and run

---
+++ install_and_run_cuberite_centos7.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo yum install -y clang git cmake make gcc glibc-devel screen libstdc*
+git clone --recursive https://github.com/cuberite/cuberite.git
+cd cuberite
+cmake . -DCMAKE_BUILD_TYPE=RELEASE
+make -j 2
+cd cuberite/Server
+echo "Don't forget to open TCP port 8080 and 25565!"
+sleep 3
+screen ./Cuberite
